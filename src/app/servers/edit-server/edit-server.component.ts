7e1276d768035c5c7c2dddd09a4ec803348784bf
Ron Hemmers <rhemmers@hotmail.com>
2023-04-23 15:02:18 -0400

    Adds retrieving query parameters and fragments

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -12,9 +13,21 @@ export class EditServerComponent implements OnInit {
   serverName = '';
   serverStatus = '';
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // Retrieve our query parameters and fragment.
+    // 1st approach... (not reactive.. will not work if changes)
+    console.log("query params:")
+    console.log(this.route.snapshot.queryParams);
+    console.log("fragment:");
+    console.log(this.route.snapshot.fragment);
+
+    // 2nd approach (allows to react to changes); don't need to unsubscribe.
+    // this.route.queryParams.subscribe();
+    // this.route.fragment.subscribe();
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
